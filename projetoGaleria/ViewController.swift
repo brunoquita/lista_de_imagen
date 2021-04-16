ed52b06ff8edf6ac7a60a84bcea0237586f27841
Bruno Rocha <brunorocha.ti2010@gmail.com>
2021-04-16 18:40:20 -0300

    Add Collection View

--- projetoGaleria/ViewController.swift
+++ projetoGaleria/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
