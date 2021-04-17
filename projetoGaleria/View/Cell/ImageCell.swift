9c1e42525fc7346a9f7ebb2dd3db7383c5378c41
Bruno Rocha <brunorocha.ti2010@gmail.com>
2021-04-16 23:08:19 -0300

    gallery configuration in view

---
+++ projetoGaleria/View/Cell/ImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCell.swift
+//  projetoGaleria
+//
+//  Created by Bruno Rocha on 16/04/21.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageCell: UIImageView!
+}
--- projetoGaleria/ViewController.swift
+++ projetoGaleria/ViewController.swift
@@ -8,15 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
+    let galleryFlowLayout = GalleryFlowLayout(
+        cellsPerRow: 4,
+        minimumInteritemSpacing: 10,
+        minimumLineSpacing: 10,
+        sectionInset: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = galleryFlowLayout
     }
-
-
+    
+    
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return  10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imgCell", for: indexPath) as! ImageCell
+        let url = URL(string: "https://images.dog.ceo/breeds/otterhound/n02091635_215.jpg")!
+        let data = try! Data(contentsOf: url)
+        cell.imageCell.image = UIImage(data: data)
+        return cell
+    }
+    
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+    
+}
